18892db7105dec6da2435330a9c105e858d9d26e
Madison Shook <ms6377@ship.engr.edu>
2025-09-16 01:04:04 -0400

    Added methods to CusumMath and fixed cusum to actually maybe work?

--- src/CusumMath.java
+++ src/CusumMath.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 // fisher yates shuffle to randomize the data in the array
@@ -13,11 +15,14 @@ import java.util.Scanner;
 public class CusumMath {
 
   private int numBootstraps;
-  private int confidenceLevel;
+  private double confidenceLevel;
+  private Random randomInt;
+  private ArrayList<Integer> results; //change to (index, confidence) pairs list
 
   public CusumMath() {
     numBootstraps = 0;
-    confidenceLevel = 0;
+    confidenceLevel = 0.0;
+    randomInt = new Random();
   }
 
   public void userData() {
@@ -25,26 +30,25 @@ public class CusumMath {
     System.out.print("How many bootstraps? : ");
     numBootstraps = scanner.nextInt();
     System.out.print("What is your confidence level? : ");
-    confidenceLevel = scanner.nextInt();
+    double num = scanner.nextDouble();
+    confidenceLevel = num / 100; //change the whole number to a percent
   }
 
   public int[] cusum(int[] arr) {
+    int mean = calcAverage(arr); //uses the overall mean
+    int[] cumulativeSum = new int[arr.length]; //array of deviations from the overall mean?
 
-    //int[] arr = {1, 2, 3, 4, 5};
-    int[] cumulativeSum = new int[arr.length];
-
-    cumulativeSum[0] = arr[0];
+    cumulativeSum[0] = arr[0] - mean;   //first element in the array is the (value - mean)
     for (int i = 1; i < arr.length; i++) {
-      cumulativeSum[i] = cumulativeSum[i - 1] + arr[i];
+      cumulativeSum[i] = cumulativeSum[i - 1] + (arr[i] - mean);
     }
 
-    // Prints the result
+    // Prints the result for now, we can delete once done
     for (int num : cumulativeSum) {
       System.out.print(num + " ");
     }
 
     return cumulativeSum;
-    //calcAverage(arr);
   }
 
   public int calcAverage(int[] array) {
@@ -57,16 +61,70 @@ public class CusumMath {
     return average;
   }
 
-  public int randomizer(int[] array) {
-    return 0;
+  public int calcSdiff(int[] array) {
+    int maxValue = array[0];   //initialize both with the first element in the array
+    int minValue = array[0];
+    int diff = 0;
+
+    for(int i = 0; i < array.length; i++){  //goes through the array once it's computed from cusum
+      maxValue = Math.max(maxValue, array[i]);
+      minValue = Math.min(minValue, array[i]);
+    }
+    diff = maxValue - minValue;
+
+    return diff;
+  }
+
+  public int[] randomizer(int[] array) {
+    int[] shuffledArray = new int[array.length];
+    int temp = 0;
+    int randomIndex = 0;
+
+    if(array == null || array.length < 2){  //if it's too small or null, there's nothing to do so just return the array
+      return array;
+    }
+    int j = 0;
+    while(j < array.length) {
+      shuffledArray[j] = array[j];   //fill the shuffledArray with the same values at the same places as array
+      j++;
+    }
+    for(int i = shuffledArray.length-1; i > 0; i--){   //start at last index, goes backwards
+      randomIndex = randomInt.nextInt(i+1);  //generate a new random index on every loop pass between 0 and i
+      temp = shuffledArray[i];
+      shuffledArray[i] = shuffledArray[randomIndex];
+      shuffledArray[randomIndex] = temp;  //swap the og value with the random new value
+    }
+    return shuffledArray;
+  }
+
+  /**
+   * might want to make into a double eventually?
+   * @param originalArray
+   * @returns Sdiff permanent
+   */
+  public int bootstrapOnce(int[] originalArray) {   //run one bootstrap iteration and return a single statistic
+    int[] shuffledArray = randomizer(originalArray); //gets a shuffled copy
+    int[] shuffledCusum = cusum(shuffledArray);
+    int Sdiff = calcSdiff(shuffledCusum);  
+    return Sdiff;
   }
 
   public int getNumBootstraps() {
     return numBootstraps;
   }
-  public int getConfidenceLevel() {
+  public double getConfidenceLevel() {
     return confidenceLevel;
   }
+
+
+
+
+
+
+
+
+
+
 
 
 
